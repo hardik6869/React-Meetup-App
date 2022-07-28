676fac4113d43fff22defd07fe3afed71c508b85
hardik6869 <sorthiyahardik6869@gmail.com>
2022-07-28 10:25:48 +0530

    Getting user input & handling form submission

--- src/components/meetups/NewMeetupForm.jsx
+++ src/components/meetups/NewMeetupForm.jsx
@@ -1,31 +1,70 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const NewMeetupForm = () => {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  const AddMeetup = (e) => {
+    e.preventDefault();
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+    console.log(meetupData);
+  };
+
   return (
     <div className="container flex justify-center mt-5">
       <div className="card col-md-5 item-center p-4 shadow-lg">
-        <h1 className="text-2xl font-weight-bold  text-center">
-          {" "}
+        <h1 className="text-2xl font-weight-bold mb-3 text-center">
           Add New Meetup
         </h1>
-        <form>
+        <form className="font-weight-bold" onSubmit={AddMeetup}>
           <div className="form-group">
-            <label htmlFor> Meetup Title</label>
-            <input type="text" id="title" className="form-control" required />
+            <label> Meetup Title</label>
+            <input
+              type="text"
+              id="title"
+              className="form-control"
+              ref={titleInputRef}
+              required
+            />
           </div>
           <div className="form-group">
-            <label htmlFor> Meetup Image</label>
-            <input type="url" id="image" className="form-control" required />
+            <label> Meetup Image</label>
+            <input
+              type="url"
+              id="image"
+              className="form-control"
+              ref={imageInputRef}
+              required
+            />
           </div>
           <div className="form-group">
-            <label htmlFor> Meetup Address</label>
-            <input type="text" id="address" className="form-control" required />
+            <label> Meetup Address</label>
+            <input
+              type="text"
+              id="address"
+              className="form-control"
+              ref={addressInputRef}
+              required
+            />
           </div>
           <div className="form-group">
-            <label htmlFor> Meetup Description</label>
+            <label> Meetup Description</label>
             <textarea
               id="description"
               className="form-control"
+              ref={descriptionInputRef}
               rows={5}
               required
             />
